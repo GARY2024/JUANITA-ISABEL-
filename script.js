d83522e68a680b2547b12e12f880a389991b38ed
GARY2024 <160370469+GARY2024@users.noreply.github.com>
2024-02-17 20:58:33 -0600

    Update script.js

--- script.js
+++ script.js
@@ -2,37 +2,37 @@
 
 const titleElement = document.querySelector(".title");
 const buttonsContainer = document.querySelector(".buttons");
-const siButton = document.querySelector(".btn--Si");
-const noButton = document.querySelector(".btn--No");
+const SiButton = document.querySelector(".btn--Si");
+const NoButton = document.querySelector(".btn--No");
 const catImg = document.querySelector(".cat-img");
 
 const MAX_IMAGES = 5;
 
 let play = true;
-let noCount = 0;
+let NoCount = 0;
 
 SiButton.addEventListener("click", handleSiClick);
 
 NoButton.addEventListener("click", function () {
   if (play) {
-    noCount++;
-    const imageIndex = Math.min(noCount, MAX_IMAGES);
+    NoCount++;
+    const imageIndex = Math.min(NoCount, MAX_IMAGES);
     changeImage(imageIndex);
     resizeSiButton();
     updateNoButtonText();
-    if (noCount === MAX_IMAGES) {
+    if (NoCount === MAX_IMAGES) {
       play = false;
     }
   }
 });
 
-function handleYesClick() {
+function handleSiClick() {
   titleElement.innerHTML = "Por favor!! :3";
   buttonsContainer.classList.add("hidden");
   changeImage("Si");
 }
 
-function resizeYesButton() {
+function resizeSiButton() {
   const computedStyle = window.getComputedStyle(SiButton);
   const fontSize = parseFloat(computedStyle.getPropertyValue("font-size"));
   const newFontSize = fontSize * 1.6;
@@ -40,7 +40,7 @@ function resizeYesButton() {
   SiButton.style.fontSize = `${newFontSize}px`;
 }
 
-function generateMessage(noCount) {
+function generateMessage(NoCount) {
   const messages = [
     "No",
     "Estas segura?",
@@ -50,7 +50,7 @@ function generateMessage(noCount) {
     " te puse michis y vamos de compras...",
   ];
 
-  const messageIndex = Math.min(noCount, messages.length - 1);
+  const messageIndex = Math.min(NoCount, messages.length - 1);
   return messages[messageIndex];
 }
 
